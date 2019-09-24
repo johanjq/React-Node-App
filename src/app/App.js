f1aa4e6bfa20516ebf7d3a31c87242d563d54a7f
Johan Quiroz <johan_9_31@hotmail.com>
2019-09-24 09:38:33 +0000

    initial state of our app

--- src/app/App.js
+++ src/app/App.js
@@ -1,79 +1,17 @@
-import React, { PureComponent } from "react";
-import { TaskList } from './components/TaskList/TaskList';
+import React, { PureComponent, Fragment } from "react";
 
 export class App extends PureComponent {
     constructor() {
         super();
         this.state = {
-            title: "",
-            description: "",
-            tasks: []
-        };
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.deleteTask = this.deleteTask.bind(this);
-    }
 
-    fetchTasks() {
-        fetch('/api/tasks')
-            .then(res => res.json())
-            .then(data => {
-                this.setState({ tasks: data });
-                console.log(this.state.tasks)
-            });
-    }
-    componentDidMount() {
-        this.fetchTasks();
-    }
-
-    handleSubmit(e) {
-        fetch("/api/tasks", {
-            method: "POST",
-            body: JSON.stringify(this.state),
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "application/json"
-            }
-        })
-            .then(res => res.json())
-            .then(data => {
-                console.log(data)
-                M.toast({ html: 'Task Saved' });
-                this.setState({ title: '', description: '' });
-                this.fetchTasks();
-            })
-            .catch(error => console.log(error));
-        e.preventDefault();
-    }
-
-    deleteTask(id) {
-        if (confirm('Are you sure you want to delete it?')) {
-            fetch(`/api/tasks/${id}`, {
-                method: 'DELETE',
-                headers: {
-                    "Content-Type": "application/json",
-                    "Accept": "application/json"
-                }
-            })
-                .then(res => res.json())
-                .then(data => {
-                    console.log(data)
-                    M.toast({ html: "Task Deleted" });
-                    this.fetchTasks();
-                })
         }
     }
-
-    handleChange(e) {
-        const { name, value } = e.target;
-        this.setState({
-            [name]: value
-        });
-    }
-
     render() {
         return (
-            <TaskList handleSubmit={this.handleSubmit} handleChange={this.handleChange} deleteTask={this.deleteTask} {...this.state} />
+            <Fragment>
+                <h1>react goes here</h1>
+            </Fragment>
         )
     }
 }
